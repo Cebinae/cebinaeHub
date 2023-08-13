65754872284fb1a0b538a120c8a76dd53387e894
Cebinae <ilyabelobrovhaha@gmail.com>
2023-08-13 20:17:14 +0300

    now try next dynamic import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,22 @@ import Greeting from '@/components/greeting'
 // import StarsBg from '@/components/technologies'
 import Image from 'next/image'
 import React from 'react'
-import CloudsBg from '@/components/cloudsBg'
+// import CloudsBg from '@/components/cloudsBg'
 import { Suspense } from 'react'
 // import PersonalFacts from '@/components/personal'
 // import StarsBg from './../components/technologies';
+import dynamic from 'next/dynamic'
 
-const StarsBg = React.lazy(() => import('@/components/technologies'));
-const Education = React.lazy(() => import('@/components/eduSection'));
-const PersonalFacts = React.lazy(() => import('@/components/personal'));
+// const StarsBg = React.lazy(() => import('@/components/technologies'));
+// const Education = React.lazy(() => import('@/components/eduSection'));
+// const PersonalFacts = React.lazy(() => import('@/components/personal'));
+
+const StarsBg = dynamic(() => import('@/components/technologies'))
+const Education = dynamic(() => import('@/components/eduSection'))
+const PersonalFacts = dynamic(() => import('./../components/cloudsBg'))
+// import CloudsBg from './../components/cloudsBg';
+// import PersonalFacts from './../components/personal';
+
 
 
 export default function Home() {
